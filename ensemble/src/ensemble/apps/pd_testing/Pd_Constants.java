3c5408c2a60f2c60785fd7d270737dcf74894ece
Pedro Bruel <pedro.bruel@gmail.com>
2013-11-01 22:12:43 -0200

    Control messages from patches are handled correctly.

--- ensemble/src/ensemble/apps/pd_testing/Pd_Constants.java
+++ ensemble/src/ensemble/apps/pd_testing/Pd_Constants.java
@@ -10,6 +10,8 @@ public final class Pd_Constants
 	public static final int BITS_PER_SAMPLE = 8 * BYTES_PER_SAMPLE;
 	public static final int INPUT_CHANNELS = 0;
 	public static final int OUTPUT_CHANNELS = 2;
+	public static final int BANG_OUTLETS = 5;
+	public static final int FLOAT_OUTLETS = 5;
 	
 	public static final float DEFAULT_SECONDS = 1;
 	/*
@@ -17,15 +19,27 @@ public final class Pd_Constants
 	 */
 	public static final String PATCH_ARGUMENT = "PATCH";
 	public static final String TICK_TARGET = "get_tick_size";
-	public static final String TICK = "send_tick_size";
-	
 	public static final String SECONDS_TARGET = "get_seconds";
-	public static final String SECONDS = "send_seconds";
-	
-	public static final String AUDIO_TOGGLE = "audio_toggle";
-	public static final String AUDIO_ON = "audio_on";
-	public static final String AUDIO_OFF = "audio_off";
-	
 	public static final String PROCESSING_ON = "processing_on";
 	public static final String PROCESSING_OFF = "processing_off";
+	public static final String BANG = "ensemble_bang";
+	public static final String FLOAT = "ensemble_float";
+	public static enum CONTROL_SYMBOL
+	{
+		TICK ( "send_tick_size" ),
+		SECONDS ( "send_seconds" ),
+		AUDIO_TOGGLE ( "audio_toggle" ),
+		AUDIO_ON ( "audio_on" ),
+		AUDIO_OFF ( "audio_off" );
+		
+		private String name;
+		private CONTROL_SYMBOL ( String new_name )
+		{
+			this.name = new_name;
+		}
+		public String get_value ( )
+		{
+			return this.name;
+		}
+	}
 }
