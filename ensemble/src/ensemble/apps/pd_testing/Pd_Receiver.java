f30a4bd55c814d7560e6b88d4919a3ae56ad4320
Pedro Bruel <pedro.bruel@gmail.com>
2013-11-06 18:39:31 -0200

    ensemble.tools.Loader is now capable of loading an app from a Pure Data patch

--- ensemble/src/ensemble/apps/pd_testing/Pd_Receiver.java
+++ ensemble/src/ensemble/apps/pd_testing/Pd_Receiver.java
@@ -8,12 +8,16 @@ public class Pd_Receiver extends PdDispatcher
 	private ArrayList< Pd_Float > floats;
 	private ArrayList< String > bangs;
 	private ArrayList< Pd_Message > messages;
+	ArrayList< String > default_control_symbols;
+	ArrayList< String > user_control_symbols;
 
 	public Pd_Receiver ( )
 	{
 		floats = new ArrayList< Pd_Float > ( );
 		bangs = new ArrayList< String > ( );
 		messages = new ArrayList< Pd_Message > ( );
+		default_control_symbols = new ArrayList< String > ( );
+		user_control_symbols = new ArrayList< String > ( );
 	}
 	@Override
 	public void print ( String pd_message ) 
@@ -35,7 +39,11 @@ public class Pd_Receiver extends PdDispatcher
 	{
 		messages.add ( new Pd_Message ( source, symbol, args ) );
 	}
-	
+	@Override
+	public void receiveSymbol ( String source, String symbol )
+	{
+		print ( "SRC=" + source + " SYM=" + symbol );
+	}
 	public ArrayList< Pd_Float > get_floats ( )
 	{
 		return floats;
@@ -47,6 +55,30 @@ public class Pd_Receiver extends PdDispatcher
 	public ArrayList< Pd_Message > get_messages ( )
 	{
 		return messages;
+	}
+	public void register_symbol ( String new_symbol )
+	{
+		user_control_symbols.add ( new_symbol );
+	}
+	public void deregister_symbol ( String target )
+	{
+		user_control_symbols.remove ( target );
+	}
+	public void register_default_symbol ( String new_symbol )
+	{
+		default_control_symbols.add ( new_symbol );
+	}
+	public void deregister_default_symbol ( String target )
+	{
+		default_control_symbols.remove ( target );
+	}
+	public ArrayList< String > get_user_symbols ( )
+	{
+		return user_control_symbols;
+	}
+	public ArrayList< String > get_default_symbols ( )
+	{
+		return default_control_symbols;
 	}
 	public void start_new_cycle ( )
 	{
