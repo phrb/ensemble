197e40cccbcf3dd03b44ca25f1200eec7e81d1d6
Pedro Bruel <pedro.bruel@gmail.com>
2014-03-05 16:40:59 -0300

    Sensors now communicate events.

--- ensemble/src/ensemble/apps/pd_testing/Pd_Sensor.java
+++ ensemble/src/ensemble/apps/pd_testing/Pd_Sensor.java
@@ -1,5 +1,6 @@
 package ensemble.apps.pd_testing;
 
+import ensemble.Event;
 import ensemble.Sensor;
 
 public class Pd_Sensor extends Sensor
@@ -9,5 +10,12 @@ public class Pd_Sensor extends Sensor
 	{
 		setEventType ( Pd_Constants.EVENT_TYPE );
 		return true;		
+	}
+	@Override
+	protected void process ( Event evt ) 
+	{
+		Pd_Audio_Buffer event = ( Pd_Audio_Buffer ) evt.objContent;
+		System.err.println ( getAgent ( ).getAgentName ( ) + ":" + getComponentName ( ) + " Received event from \"" + event.get_source ( ) + "\" at \"" + event.get_pd_time_tag ( ) + "\"." );
+		
 	}
 }
