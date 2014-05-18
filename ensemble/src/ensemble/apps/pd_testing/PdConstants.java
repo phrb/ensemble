a70c2934290a5911f62d8fc5c4fa95a71e80eb59
Pedro Bruel <pedro.bruel@gmail.com>
2014-05-18 14:05:18 -0300

    Sensors now receive and play samples.
    Samples received by an Agent are written to Sensor
    arrays in a patch, and played with tabread4~ and
    line~. Block size and sample playing speed are
    still hardcoded in the abstractions.

--- ensemble/src/ensemble/apps/pd_testing/PdConstants.java
+++ ensemble/src/ensemble/apps/pd_testing/PdConstants.java
@@ -14,20 +14,17 @@ public final class PdConstants
 	public static final int INPUT_CHANNELS = 1;
 	public static final int OUTPUT_CHANNELS = 1;	
 	
-	public static final int DEFAULT_TICKS = 32;
+	public static final int DEFAULT_TICKS = 256;
 	public static final int DEFAULT_SAMPLES_PER_BUFFER = PD_BLOCK_SIZE;
-	/*
-	 * Text:
-	 */
+
 	public static final String SEPARATOR = ":";
 	public static final String AGENT_CLASS = "ensemble.apps.pd_testing.PdAgent";
 	public static final String ACTUATOR_CLASS = "ensemble.apps.pd_testing.PdActuator";
 	public static final String SENSOR_CLASS = "ensemble.apps.pd_testing.PdSensor";
 	public static final String REASONING_CLASS = "ensemble.apps.pd_testing.PdReasoning";
 	public static final String DEFAULT_REASONING_NAME = "Reasoning";
-	/* 
-	 * Ensemble Methods:
-	 */
+	public static final String TABREAD4_SIGNAL = ":play";
+
 	public static final String MULTIPLY_FACT = "*";
 	public static final String ADD_TO_FACT = "+";
 	public static final String UPDATE_FACT = "update_fact";
@@ -39,9 +36,6 @@ public final class PdConstants
 	public static final String ADD_REASONING = "add_reasoning";
 	public static final String SUBSCRIPTION = "subscribe";
 	public static final String UNSUBSCRIPTION = "unsubscribe";
-	/* 
-	 * Arguments:
-	 */
 	public static final String SELF_ACTUATOR = "actself";
 	public static final String SELF_SENSOR = "senseself";
 	public static final String BANG = "bang";
@@ -61,13 +55,8 @@ public final class PdConstants
 	public static final String SCOPE = "scope";
 	public static final String AVATAR_SENSOR = "avatar:sensor";
 	public static final String AVATAR_ACTUATOR = "avatar:actuator";
-	/*
-	 * Ensemble will send bangs to these symbols:
-	 */
+
 	public static final String START_AGENT = "start";
 	public static final String PROCESSING_ON = "processing_on";
 	public static final String PROCESSING_OFF = "processing_off";
-	/*
-	 * Ensemble will subscribe to these symbols:
-	 */
 }
