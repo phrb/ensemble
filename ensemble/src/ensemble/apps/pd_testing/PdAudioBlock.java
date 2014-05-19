4444e90d6bc2cd61f4300655f705ab7c66553e1d
Pedro Bruel <pedro.bruel@gmail.com>
2014-05-19 18:35:10 -0300

    read_memory~ now works.
    Still pending: memory offset changes (seconds?)

--- ensemble/src/ensemble/apps/pd_testing/PdAudioBlock.java
+++ ensemble/src/ensemble/apps/pd_testing/PdAudioBlock.java
@@ -4,12 +4,10 @@ public class PdAudioBlock
 {
 	private float[ ] samples;
 	private String source;
-	private String target;
 	public PdAudioBlock ( float[ ] new_samples, String new_source )
 	{
 		samples = new_samples;
 		source = new_source;
-		target = null;
 	}
 	public float[ ] get_samples ( )
 	{
@@ -19,16 +17,8 @@ public class PdAudioBlock
 	{
 		return source;
 	}
-	public String get_target ( )
-	{
-		return target;
-	}
 	public void set_source ( String new_source )
 	{
 		source = new_source;
-	}
-	public void set_target ( String new_target )
-	{
-		target = new_target;
 	}
 }
